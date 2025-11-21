e183d9313844d4d6256fc4b354ac38de8cfd68f0
Krishiv <97694241+Krishiv23@users.noreply.github.com>
2025-11-21 11:47:35 +0530

    Enhance built-in command functions in buildins.c
    Refactor built-in commands to handle directory changes and exit status.

--- src/buildins.c
+++ src/buildins.c
@@ -1,19 +1,78 @@
+#include "buildins.h"
+#include <ctype.h>
+#include <errno.h>
+#include <linux/limits.h>
 #include <stdio.h>
 #include <string.h>
-#include <dirent.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include "buildins.h"
+
+char last_dir[PATH_MAX]; //initialized to 0
+int exit_stat;
 
 int bi_cd(int argc, char** argv){
+    char cur_dir[PATH_MAX];
+    char path[PATH_MAX];
+
+    if(!getcwd(cur_dir, sizeof(cur_dir))) *cur_dir = '\0';
+    if(argv[1]==NULL && argc<=2){
+        strcpy(path, getenv("HOME"));
+        if(argv[1]==NULL){ 
+            fprintf(stderr,"Home is not set");
+            return 1;
+        }
+    }else if(strcmp(argv[1], "-")==0){
+        if(*last_dir==0){
+            fprintf(stderr, "No previous directory");
+            return 1;
+        }
+        strcpy(path, last_dir);
+    }else{
+        if(argv[1][0] == '~'){
+            if(argv[1][1]=='/' || argv[1][1]=='\0'){
+                char* home=getenv("HOME");
+                if(!home){
+                    fprintf(stderr,"Home is not set");
+                    return 1;
+                }
+
+                snprintf(path, sizeof(path), "%s%s", home, ((argv[1])+1));
+            }else{
+                fprintf(stderr, "cd: %s","no such file or directory");
+                return 1;
+            }
+        }
+    }
+
+    if(chdir(path)!=0){
+        fprintf(stderr, "cd: %s: %s\n", path, strerror(errno));
+        return 1;
+    }
+
+    strcpy(last_dir, cur_dir);
+
     return 0;
 }
 
 int bi_echo(int argc, char** argv){
+    if(argv[1]==NULL) fprintf(stdout, "\n");
+    else fprintf(stdout, "%s\n", argv[1]);
     return 0;
 }
 
 int bi_exit(int argc, char** argv){
-    return 0;
+    if(argc>2){
+        fprintf(stderr, "exit: %s", "Too many arguments");
+        return 1;
+    }
+
+    if(argv[1]!=NULL){
+        if(isdigit(argv[1][0]-'0')){
+            exit_stat = argv[1][0]-'0';
+            exit(exit_stat);
+        }
+    }
+    exit(exit_stat);
 }
 
 int bi_pwd(int argc, char** argv){
