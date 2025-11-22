7c1f673accc28d565661c176676955081676d510
Krishiv <97694241+Krishiv23@users.noreply.github.com>
2025-11-22 13:17:36 +0530

    Improved error handling in bi_cd and bi_ls functions
    1. Implemented the basic ls command
    2. Refactord bi_cd function for better error handling and initialization.

--- src/buildins.c
+++ src/buildins.c
@@ -1,23 +1,32 @@
 #include "buildins.h"
 #include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
+#include <stdlib.h>
 #include <linux/limits.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 char last_dir[PATH_MAX]; //initialized to 0
 int exit_stat;
 
 int bi_cd(int argc, char** argv){
-    char cur_dir[PATH_MAX];
-    char path[PATH_MAX];
+    char cur_dir[PATH_MAX]={0};
+    char path[PATH_MAX]={0};
+
+    if(argc>3){
+        fprintf(stderr, "Too many arguments\n");
+        return 1;
+    }
 
     if(!getcwd(cur_dir, sizeof(cur_dir))) *cur_dir = '\0';
+
     if(argv[1]==NULL && argc<=2){
         strcpy(path, getenv("HOME"));
-        if(argv[1]==NULL){ 
+        if(*path == 0){ 
             fprintf(stderr,"Home is not set");
             return 1;
         }
@@ -37,10 +46,16 @@ int bi_cd(int argc, char** argv){
                 }
 
                 snprintf(path, sizeof(path), "%s%s", home, ((argv[1])+1));
-            }else{
-                fprintf(stderr, "cd: %s","no such file or directory");
-                return 1;
             }
+        }else if(strcmp(argv[1], "..")==0){
+            strcpy(path, argv[1]);
+        }else if(argv[1][0] == '.'){
+            if(argv[1][1]=='/' || argv[1][1]=='\0'){
+                snprintf(path, sizeof(path), "%s%s", cur_dir, ((argv[1])+1));
+            }
+        }else{
+            fprintf(stderr, "cd: %s","no such file or directory");
+            return 1;
         }
     }
 
@@ -72,7 +87,10 @@ int bi_exit(int argc, char** argv){
             exit(exit_stat);
         }
     }
-    exit(exit_stat);
+
+    //under construction
+
+    return exit_stat;
 }
 
 int bi_pwd(int argc, char** argv){
@@ -85,6 +103,58 @@ int bi_pwd(int argc, char** argv){
 }
 
 int bi_ls(int argc, char** argv){
+    DIR* dir;
+    struct dirent* entry;
+
+    char flag[100]={0};
+    char path[PATH_MAX]={0};
+
+    if(argc>3){
+        fprintf(stderr, "Too many arguments");
+        return 1;
+    }
+
+    for(int i=1; i<argc; ++i){
+        if(argv[i][0]=='-'){
+            strcpy(flag, argv[i]);
+        }else{
+            strcpy(path, argv[i]);
+        }
+    }
+    
+    if(*path != 0){
+        dir=opendir(path);
+    }else{
+        strcpy(path, ".");
+        dir=opendir(path);
+    }
+
+    if(!dir){
+        fprintf(stderr, "ls: cannot access '%s'\n", path);
+        return 1;
+    }
+
+    if(*flag != 0){
+        if(flag[1]=='a'){
+            while((entry = readdir(dir))!=NULL){
+                fprintf(stdout, "%s\n", entry->d_name);
+            }
+        }else{
+            fprintf(stderr, "ls: invalid option --%c\n", flag[1]);
+            return 1;
+        }
+    }else{
+        while((entry=readdir(dir))!=NULL){
+            if(*(entry->d_name) != '.'){
+                fprintf(stdout, "%s\n", entry->d_name);
+            }
+        }
+    }
+
+    if(closedir(dir)!=0){
+        fprintf(stderr, "error closing the directory\n");
+        return 1;
+    }
     return 0;
 }
 
